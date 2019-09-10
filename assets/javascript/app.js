51c522005ec6e46b9663ea1333f6375a55814933
Michael Johnston <mjjohnston55@gmail.com>
2019-09-09 21:27:18 -0700

    most of table and table input form code done. Need to link to market API and input data

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,83 @@
 // positions functionality
-var initialPrice;
-var finalPrice;
-var shares;
-var valueChange;
-var valueChangePercent;
-var sign;
 
-initialPrice = 10;
-finalPrice = 5;
-var shares = 1;
+// database information
 
-setInterval(function(){ 
+    // TO DO
+// var config = {
+//     apiKey:,
+//     authDomain:,
+//     databaseURL:,
+//     storageBucket:
+//   };
+  
+  firebase.initializeApp(config);
+  
+  var database = firebase.database();
 
-        // recalculating for updating value    
-        valueChange = ((finalPrice* shares) - (initialPrice * shares));
-        valueChangePercent = ((finalPrice/initialPrice) * 100);
-        if (initialPrice >= finalPrice) {
-            sign = "-";
-        }
-        else sign = "";
+// Button to add new stock
+$("#add-position-btn").on("click", function(event) {
+    event.preventDefault();
     
-        $("#initial-price-div").html("Initial Price: " + initialPrice);
-        $("#final-price-div").html("New Price: " + finalPrice);
-        $("#change-div").html("Value Change: " + valueChange);
-        $("#percent-change-div").html("Percentage Change: " + valueChangePercent);
+    var stockName = $("#input-name-div").val().trim();
+    var initialPrice = $("#input-initial-price-div").val().trim();
+    var numShares = $("#input-shares-div").val().trim();
 
-        // Incrementing finalPrice for testing purposes only -- real functionality will be merely updating finalPrice to current real value
-        finalPrice++;
+    // create local temporary object for new stock input
+    var newStock = {
+        name: stockName,
+        startPrice: initialPrice,
+        shares: numShares
+      };
+
+      database.ref().push(newStock);
+
+    //   displays new object in console
+      console.log(newStock.name);
+      console.log(newStock.startPrice);
+      console.log(newStock.shares);
+
+      alert("Stock successfully added");
+
+        // Clears all of the text-boxes
+        $("#input-name-div").val("");
+        $("#input-initial-price-div").val("");
+        $("#input-final-price-div").val("");
+        $("#input-shares-div").val("");
     
-    ; }, 3000);
+    });
+
+//  create Firebase event for adding a stock to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+
+      // stores everything into a variable
+  var stockName = childSnapshot.val().name;
+  var initialPrice = childSnapshot.val().startPrice;
+  var numShares = childSnapshot.val().shares;
+
+    // logging stock info
+    console.log(stockName);
+    console.log(initialPrice);
+    console.log(numShares);
 
 
+        // (re)calculating for updating value    (Inputting finalPrice from API - TODO)
+        // var valueChange = ((finalPrice* shares) - (initialPrice * shares));
+        // var valueChangePercent = ((finalPrice/initialPrice) * 100);
+        // if (initialPrice >= finalPrice) {
+        //     var sign = "-";
+        // }
+        // else var sign = "";
 
+        var newRow = $("<tr>").append(
+            $("<td>").text(stockName),
+            $("<td>").text(initialPrice),
+            $("<td>").text(numShares),
+            $("<td>").text(finalPrice),
+            $("<td>").text(sign + valueChange),
+            $("<td>").text(sign + valueChangePercent)
+          );
 
+  // Append the new row to the table
+  $("#positions-table > tbody").append(newRow);
+});
