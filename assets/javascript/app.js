5d60ef042a66a127b4dd59b32286c10933818302
Michael Johnston <mjjohnston55@gmail.com>
2019-09-14 12:51:42 -0700

    all 73 console errors fixed. still very buggy but basic table and database connection works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,92 +10,115 @@ var config = {
     appId: "1:324258865048:web:529a40578f4322516755ed"
   };
 
-
-
-
+      // declaring to-be-extrapolated variables
+      var newPrice;
+      var valueChange;
+      var valueChangePercent;
+      var sign;
+      var numShares;
   
   firebase.initializeApp(config);
   
-  var database = firebase.database();
+  var database = firebase.database().ref();
 
 // Button to add new stock
 $("#add-position-btn").on("click", function(event) {
     event.preventDefault();
 
-  
-    request.send()
-    
-    var stockName = $("#input-name-div").val().trim();
+    // setting variables from user input
+    var stockSymbol = $("#input-symbol-div").val().trim();
     var initialPrice = $("#input-initial-price-div").val().trim();
     var numShares = $("#input-shares-div").val().trim();
+    // get request from market API (placeholder API for now)
+    var queryURL = "https://api.worldtradingdata.com/api/v1/stock?symbol=" + stockSymbol + "&api_token=E9WkYOdirs63yl0rDrZNAnAfzIT9JEoTVrjDDedq2xeTTeROg4ZvtJy8HCUC"
+    var newPrice;
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+
+    })
+
+    
+      .then(function(data) {
+      newPrice = data.data[0].price;
+      console.log(newPrice);
+      // console.log(data);
+
+
+    
+
 
           // (Inputting finalPrice from API - TODO)
   //  INPUT FROM API HERE
 
           // (re)calculating extrapolated variables for current input    
-    // var valueChange = ((finalPrice* shares) - (initialPrice * shares));
-    // var valueChangePercent = ((finalPrice/initialPrice) * 100);
-    // if (initialPrice >= finalPrice) {
-    //     var sign = "-";
-    //     }
-    // else var sign = "";
+    valueChange = ((newPrice* numShares) - (initialPrice * numShares));
+    valueChangePercent = ((newPrice/initialPrice) * 100);
+    if (initialPrice >= newPrice) {
+        sign = "-";
+        }
+    else sign = "+";
+
+    console.log(newPrice);
 
 
     // create local temporary object for new stock input
     var newStock = {
-        name: stockName,
+        name: stockSymbol,
         startPrice: initialPrice,
-        shares: numShares
+        shares: numShares,
+        finalPrice: newPrice,
+        change: valueChange,
+        changePercent: valueChangePercent,
+        valueSign: sign
       };
 
-      database.ref().push(newStock);
+      console.log(newPrice);
+      database.push(newStock);
 
     //   displays new object in console
-      console.log(newStock.name);
-      console.log(newStock.startPrice);
-      console.log(newStock.shares);
+      console.log(newStock);
 
       alert("Stock successfully added");
 
         // Clears all of the text-boxes
-        $("#input-name-div").val("");
+        $("#input-symbol-div").val("");
         $("#input-initial-price-div").val("");
         $("#input-shares-div").val("");
     
     });
 
 //  create Firebase event for adding a stock to the database and a row in the html when a user adds an entry
-database.ref().on("child_added", function(childSnapshot) {
+database.on("child_added", function(childSnapshot) {
     console.log(childSnapshot.val());
 
       // stores everything into a variable
-  var stockName = childSnapshot.val().name;
-  var initialPrice = childSnapshot.val().startPrice;
-  var numShares = childSnapshot.val().shares;
+      stockSymbol = childSnapshot.val().name;
+      initialPrice = childSnapshot.val().startPrice;
+      numShares = childSnapshot.val().shares;
+      newPrice = childSnapshot.val().finalPrice;
+      valueChange = childSnapshot.val().change;
+      valueChangePercent = childSnapshot.val().changePercent;
+      sign = childSnapshot.val().valueSign;
 
     // logging stock info
-    console.log(stockName);
+    console.log(stockSymbol);
     console.log(initialPrice);
     console.log(numShares);
 
 
-        // (re)calculating for updating value    (Inputting finalPrice from API - TODO)
-        // var valueChange = ((finalPrice* shares) - (initialPrice * shares));
-        // var valueChangePercent = ((finalPrice/initialPrice) * 100);
-        // if (initialPrice >= finalPrice) {
-        //     var sign = "-";
-        // }
-        // else var sign = "";
-
         var newRow = $("<tr>").append(
-            $("<td>").text(stockName),
+            $("<td>").text(stockSymbol),
             $("<td>").text(initialPrice),
             $("<td>").text(numShares),
-            $("<td>").text(finalPrice),
+            $("<td>").text(newPrice),
             $("<td>").text(sign + valueChange),
             $("<td>").text(sign + valueChangePercent)
           );
 
   // Append the new row to the table
-  $("#positions-table > tbody").append(newRow);
+  $("#positions-table > tbody").after(newRow);
+});
+
 });
