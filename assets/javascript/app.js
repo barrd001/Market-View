e9b13e76c85f1cc03dce460f6bd58279717bf907
thnlsn <52093425+thnlsn@users.noreply.github.com>
2019-09-16 19:29:49 -0700

    re-styled

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,4 @@
-/* 
-console.log($(".input-3lfOzLDc-").val())
-
-
-
-$('#search-button').on('click', function(event) {
-    event.preventDefault();
-    var searchBar = $('.input-3lfOzLDc').val().trim();
-$(".input-3lfOzLDc").val(searchBar);
-var input = $(".input-3lfOzLDc-");
-console.log(input);
-/* console.log(searchBar);
- 
-});
- */
-
-
-/* var userSearch = "CPB";
-
-
-$('#search-button').on('click', function(event) {
-    event.preventDefault();
-    var searchBar = $('#search-bar').val().trim();
-    userSearch = searchBar;
- 
-    console.log(searchBar);
-    console.log(userSearch);
-}); */
+$('#sign-in').on('click', function () {
+  event.preventDefault();
+  $('.modal').modal('toggle')
+ })
